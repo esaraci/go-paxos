75dbf60a5f630bb87b95b2de615711de6e000c0b
Eugen Saraci <eugen.saraci@gmail.com>
2020-01-14 10:47:00 +0100

    busty timeout reinstated

--- paxos/queries/sqlite-queries.go
+++ paxos/queries/sqlite-queries.go
@@ -22,7 +22,7 @@ var db *sql.DB
 func SQLitePrepareDBConn() {
 
 	// _busy_timeout=5000&
-	db, _ = sql.Open(sqlDriver, "file:database.db?cache=shared&mode=rwc")
+	db, _ = sql.Open(sqlDriver, "file:database.db?_busy_timeout=5000&cache=shared&mode=rwc")
 	db.SetMaxOpenConns(1)
 	//_, _ = db.Exec("PRAGMA journal_mode=WAL")
 }
